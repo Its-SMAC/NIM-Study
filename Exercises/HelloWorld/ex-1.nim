a1ca0b36021aaa68a3c0a65c920f8027f93e8bec
SMAC <santiagocosta.smac@gmail.com>
2025-12-04 10:15:26 +0000

    Create the fisrt exercise
    Creation of first exercise to show comments and how you print a message on NIM

---
+++ Exercises/HelloWorld/ex-1.nim
@@ -0,0 +1,29 @@
+# Hello World Exercise 1
+
+#[
+  To start that exercise, we go learn how make a comment, before that text you can saw a # behind the text, this is a comment line.
+  But, if you want a comment block, you can put "#[ Text inside ]#" and chan nah, you alright write a comment block.
+  In addition you have a 'discard' command, you put discard""" And you text go here, in the middle. """.
+
+  Fine, after you discover how make a comment, lets do tha main exercise, it's the beggining of any language, a simple, fast and quick exercise.
+]#
+
+#The main printer in NIM, is 'echo' command:
+
+echo "Hello World"
+echo("Hello World")
+"Hello".echo "World"
+"Hello World".echo
+
+# However, exist other form to make this:
+
+write(stdout,"Hello World\n")
+
+#[
+  To right it done and test, open your terminal and write "nim r c NameFile.nim" and run.
+  Explain about "nim r c NameFile.nim":
+    "nim" is to say tha language you go use.
+    "r" is to compile and run on a single command.
+    "c" specifies which language you want to compile.
+    "NameFile.nim" say which file you want make this process
+]#
